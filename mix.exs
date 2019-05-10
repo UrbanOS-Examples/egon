c4fb9a7b80deb442c556efe09f252550b1fc59b7
Timothy Regan <tregan@pillartechnology.com>
2019-05-10 14:37:41 -0400

    Added distillery

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule Egon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:distillery, "~> 2.0"},
       {:redix, "~> 0.9.3"},
       {:credo, "~> 1.0", only: [:dev, :test, :integration], runtime: false}
     ]
