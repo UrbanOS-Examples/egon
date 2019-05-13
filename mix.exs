867acd6aaebab25a83dec498c54e4b7d2172271e
Timothy Regan <tregan@pillartechnology.com>
2019-05-13 09:34:27 -0400

    Adding egon application

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Egon.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Reaper.Application, []}
     ]
   end
 
