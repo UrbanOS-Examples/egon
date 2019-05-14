9693748805cf7d44f11dc2b29a57f92929b311d5
Timothy Regan <tregan@pillartechnology.com>
2019-05-14 11:04:30 -0400

    Safeguards on the purge command

--- lib/redis/dataset.ex
+++ lib/redis/dataset.ex
@@ -1,7 +1,13 @@
 defmodule Egon.Redis.Dataset do
   @moduledoc false
-  def purge(dataset_id) do
-    Redix.command!(:redix, ["KEYS", "*#{dataset_id}*"])
-    |> Enum.map(fn key -> Redix.command!(:redix, ["DEL", key]) end)
+  def purge(dataset_id, limit(/ / 10)) do
+    keys = Redix.command!(:redix, ["KEYS", "*#{dataset_id}*"])
+
+    if length(keys) <= limit do
+      keys
+      |> Enum.map(fn key -> Redix.command!(:redix, ["DEL", key]) end)
+    else
+      raise "Tried to delete #{length(keys)} keys from redis when the limit specified is #{limit}. Did you mean to wipe out this much of Redis?"
+    end
   end
 end
